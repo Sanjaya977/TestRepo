f9406d57b698b16896a8358c24ab7e57e837a62d
Sanjaya Raut <sanjaya.raut@nepasoft.com>
2020-02-19 11:20:02 +0545

    Initial Commit

---
+++ TeamcityTest/TeamcityTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamcityTest
+{
+    public class Class1
+    {
+    }
+}
